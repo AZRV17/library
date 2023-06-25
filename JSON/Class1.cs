942c5d830e0ef528aa6d1d7a86f52a4d52dc9063
AZRV17 <alex170805azrov@gmail.com>
2023-06-25 19:21:15 +0300

    commit

---
+++ JSON/Class1.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+
+namespace JSON
+{
+    public class Class1
+    {
+        public static void SaveToJson<T>(T data)
+        {
+            string filePath = "C:/Users/alexa/OneDrive/Рабочий стол/Практическая/WPFLib/WPFLib/data.json";
+
+            List<T> dataList = new List<T>();
+
+            if (File.Exists(filePath))
+            {
+                string jsonData = File.ReadAllText(filePath);
+                dataList = JsonConvert.DeserializeObject<List<T>>(jsonData);
+            }
+
+            dataList.Add(data);
+
+            string updatedJsonData = JsonConvert.SerializeObject(dataList, Formatting.Indented);
+            File.WriteAllText(filePath, updatedJsonData);
+        }
+
+        public static List<T> LoadFromJson<T>()
+        {
+            string filePath = "C:/Users/alexa/OneDrive/Рабочий стол/Практическая/WPFLib/WPFLib/data.json";
+
+            if (File.Exists(filePath))
+            {
+                string jsonData = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<List<T>>(jsonData);
+            }
+
+            return new List<T>();
+        }
+    }
+}
---
+++ WPFLib/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using JSON;
+
+namespace WPFLib
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            string input = TextBox.Text;
+            List<Class2> s = Class1.LoadFromJson<Class2>();
+            foreach (Class2 c in s)
+            {
+                if (c.name == input) 
+                {
+                    Name.Text = c.name;
+                    Age.Text = c.age.ToString();
+                }
+            }
+        }
+    }
+
+    internal class Class2
+    {
+        public string name;
+        public int age;
+    }
+}
